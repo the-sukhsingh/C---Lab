679c35b3ac1b27294ae56fc2e67635f6df251882
Sukhjit <sukhaji65@gmail.com>
2024-10-07 23:17:02 +0530

    Refactor .gitignore file to exclude executable files and output directory

---
+++ Linked List/Traverse.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+typedef struct node
+{
+    int info;
+    node* link;
+} node;
+
+void traverse(node* start)
+{
+    node* ptr = start;
+    while (ptr != NULL)
+    {
+        cout << ptr->info << endl;
+        ptr = ptr->link;
+    }
+}
+
+
+int main(){
+    node n1,n2,n3;
+    n1.info = 45;
+    n1.link = &n2;
+    n2.info = 50;
+    n2.link = &n3;
+    n3.info = 55;
+    n3.link = NULL;
+    node* start = &n1;
+    traverse(start);
+    return 0;
+}
